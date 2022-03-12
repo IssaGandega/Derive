9227c6ad01e548a0a4366d2784015c41a68673b6
Lucrezia <lucrezia.romeo@gmail.com>
2022-03-12 22:05:11 +0100

    tweening UI wip

--- Assets/UI/Anim_Scripts/UI_Rotate.cs
+++ Assets/UI/Anim_Scripts/UI_Rotate.cs
@@ -6,18 +6,13 @@ public class UI_Rotate : MonoBehaviour
 
 {
 
-    [SerializeField] float x;
-    [SerializeField] float y;
-    [SerializeField] float z;
-    [SerializeField] float time_size;
     [SerializeField] float z_rotation;
     [SerializeField] float time_rotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.LeanScale(new Vector3(x, y, z), time_size).setEaseInSine().setLoopPingPong();
-        LeanTween.rotateAroundLocal(gameObject, Vector3.forward, z_rotation, time_rotation).setRepeat(-1);
+        LeanTween.rotateAroundLocal(gameObject, Vector3.forward, z_rotation, time_rotation).setEaseInQuad().setLoopPingPong();
     }
 
     // Update is called once per frame
