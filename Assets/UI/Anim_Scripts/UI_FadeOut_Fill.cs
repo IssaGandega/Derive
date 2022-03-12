09feef39a16bdcc7dd740352f95dfd31d1b428c2
Lucrezia <lucrezia.romeo@gmail.com>
2022-03-12 22:54:14 +0100

    tjr weep

---
+++ Assets/UI/Anim_Scripts/UI_FadeOut_Fill.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_FadeOut_Fill : MonoBehaviour
+{
+public Image image;
+    [SerializeField] float tweentime, start_fill, end_fill, delay;
+    [SerializeField] Color beginColor, endColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.fillAmount = 0;
+        LeanTween.value(gameObject, start_fill, end_fill, tweentime)
+            .setEaseInOutBack()
+            .setOnUpdate((value) =>
+            {
+                image.fillAmount = value;
+                image.color = Color.Lerp(beginColor, endColor, value);
+            })
+            .setDelay(delay);
+
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
