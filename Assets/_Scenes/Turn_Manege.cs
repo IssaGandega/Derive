c00ad99fa9b2f685dbc7830e5023aa4276c784a6
Lucrezia <lucrezia.romeo@gmail.com>
2022-04-08 18:37:10 +0200

    ignorezmwatjr

--- Assets/_Scenes/Turn_Manege.cs
+++ Assets/_Scenes/Turn_Manege.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class Turn_Manege : MonoBehaviour
 {
 
-    [SerializeField] float z_rotation;
+    [SerializeField] float rotation_degrees;
     [SerializeField] float time_rotation;
+    [SerializeField] float up_max_position;
+    [SerializeField] float time_bounce;
+    [SerializeField] Vector3 stretch_squash;
 
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.rotateLocal(gameObject, z_rotation, time_rotation).setEaseInOutCubic();
+
+        // ! remove looping
+
+        //rotation
+        LeanTween.rotateAround(gameObject,Vector3.up, rotation_degrees, time_rotation).setEaseOutCubic().setLoopCount(-1);
+        
+        //bounce
+        LeanTween.moveLocalY(gameObject, up_max_position, time_bounce).setEasePunch().setLoopCount(-1);
+
+        //stretch&squash
+        LeanTween.scale(gameObject,stretch_squash, 1f).setEasePunch().setLoopCount(-1) ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0)) // un seul doigt sur l'écran
-        {
 
-        }
+
     }
 }
